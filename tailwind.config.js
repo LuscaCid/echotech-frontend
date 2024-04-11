3eeef12e1f45e6a8224fa9c0897f5327cf2c5231
Lucas Cid <lucasfelipaaa@gmail.com>
2024-04-10 21:41:31 -0300

    adds tailwindcss and singup page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode : 'class',
+  content: [
+    "./*.{html}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx,html}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx,html}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx,html}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx,html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
